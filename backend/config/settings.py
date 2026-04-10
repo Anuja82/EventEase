c96988875478bd50989911cfff847426a443e4ed
Anuja R <anujar5678@gmail.com>
2026-04-10 11:10:33 +0530

    update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -101,6 +101,9 @@ DATABASES = {
     )
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "https://eventease-blsjdzmfe-anuja82s-projects.vercel.app",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
